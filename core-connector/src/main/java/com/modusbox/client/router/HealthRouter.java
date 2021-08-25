05da9fbb7d61c7be5f2df97705333928ce7470fc
Yordan Stoykov <yordan.stoykov@modusbox.com>
2021-08-25 14:53:21 +0300

    Added CORS to health endpoint

--- core-connector/src/main/java/com/modusbox/client/router/HealthRouter.java
+++ core-connector/src/main/java/com/modusbox/client/router/HealthRouter.java
@@ -1,6 +1,7 @@
 package com.modusbox.client.router;
 
 import com.modusbox.client.exception.RouteExceptionHandlingConfigurer;
+import com.modusbox.client.processor.CorsFilter;
 import io.prometheus.client.Counter;
 import io.prometheus.client.Histogram;
 import org.apache.camel.Exchange;
@@ -9,6 +10,7 @@ import org.apache.camel.builder.RouteBuilder;
 public class HealthRouter extends RouteBuilder {
 
 	private final RouteExceptionHandlingConfigurer exception = new RouteExceptionHandlingConfigurer();
+	private final CorsFilter corsFilter = new CorsFilter();
 
 	private static final String ROUTE_ID = "com.modusbox.getHealth";
 	private static final String COUNTER_NAME = "counter_get_health_requests";
@@ -30,7 +32,9 @@ public class HealthRouter extends RouteBuilder {
 		// Add custom global exception handling strategy
 		exception.configureExceptionHandling(this);
 
-        from("direct:getHealth").routeId(ROUTE_ID).doTry()
+
+
+		from("direct:getHealth").routeId(ROUTE_ID).doTry()
 			.process(exchange -> {
 				requestCounter.inc(1); // increment Prometheus Counter metric
 				exchange.setProperty(TIMER_NAME, requestLatency.startTimer()); // initiate Prometheus Histogram metric
@@ -40,6 +44,8 @@ public class HealthRouter extends RouteBuilder {
 			/*
 			 * BEGIN processing
 			 */
+			// Add CORS headers
+			.process(corsFilter)
 			.setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200))
 			.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
 			.setBody(constant("OK"))
