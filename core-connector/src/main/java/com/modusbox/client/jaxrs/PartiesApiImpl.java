5b6b9cd6237fc64d3b84b01bb6e4eeaafa442029
Yordan Stoykov <yordan.stoykov@modusbox.com>
2021-09-07 11:27:05 +0300

    Added support for GET parties by sub Id

--- core-connector/src/main/java/com/modusbox/client/jaxrs/PartiesApiImpl.java
+++ core-connector/src/main/java/com/modusbox/client/jaxrs/PartiesApiImpl.java
@@ -11,4 +11,9 @@ public class PartiesApiImpl implements PartiesApi {
     public TransferPartyInbound getPartiesByIdTypeIdValue(String idType, @Size(min = 1, max = 128) String idValue) {
         return null;
     }
+
+    @Override
+    public TransferPartyInbound getPartiesByIdTypeIdValueIdSubValue(String idType, @Size(min = 1, max = 128) String idValue, @Size(min = 1, max = 128) String idSubValue) {
+        return null;
+    }
 }
--- core-connector/src/main/java/com/modusbox/client/router/PartiesRouter.java
+++ core-connector/src/main/java/com/modusbox/client/router/PartiesRouter.java
@@ -13,6 +13,7 @@ public class PartiesRouter extends RouteBuilder {
 	private final RouteExceptionHandlingConfigurer exception = new RouteExceptionHandlingConfigurer();
 	private final CorsFilter corsFilter = new CorsFilter();
 
+	// Prometheus metrics for GET /parties/{idType}/{idValue}
 	private static final String ROUTE_ID = "com.modusbox.getPartiesByIdTypeIdValue";
 	private static final String COUNTER_NAME = "counter_get_parties_requests";
 	private static final String TIMER_NAME = "histogram_get_parties_timer";
@@ -20,12 +21,28 @@ public class PartiesRouter extends RouteBuilder {
 
 	public static final Counter requestCounter = Counter.build()
 			.name(COUNTER_NAME)
-			.help("Total requests for GET /parties.")
+			.help("Total requests for GET /parties/{idType}/{idValue}.")
 			.register();
 
 	private static final Histogram requestLatency = Histogram.build()
 			.name(HISTOGRAM_NAME)
-			.help("Request latency in seconds for GET /parties.")
+			.help("Request latency in seconds for GET /parties/{idType}/{idValue}.")
+			.register();
+
+	// Prometheus metrics for GET /parties/{idType}/{idValue}/{idSubValue}
+	private static final String ROUTE_ID_SUB = "com.modusbox.getPartiesByIdTypeIdValueSubIdValue";
+	private static final String COUNTER_NAME_SUB = "counter_get_parties_subId_requests";
+	private static final String TIMER_NAME_SUB = "histogram_get_parties_subId_timer";
+	private static final String HISTOGRAM_NAME_SUB = "histogram_get_parties_subId_requests_latency";
+
+	public static final Counter requestCounterSub = Counter.build()
+			.name(COUNTER_NAME_SUB)
+			.help("Total requests for GET /parties/{idType}/{idValue}/{idSubValue}.")
+			.register();
+
+	private static final Histogram requestLatencySub = Histogram.build()
+			.name(HISTOGRAM_NAME_SUB)
+			.help("Request latency in seconds for GET /parties/{idType}/{idValue}/{idSubValue}.")
 			.register();
 
 	public void configure() {
@@ -69,6 +86,45 @@ public class PartiesRouter extends RouteBuilder {
 						"'Send response, " + ROUTE_ID + "', null, null, 'Output Payload: ${body}')") // default logging
 				.doFinally().process(exchange -> {
 			((Histogram.Timer) exchange.getProperty(TIMER_NAME)).observeDuration(); // stop Prometheus Histogram metric
+		}).end()
+		;
+
+		from("direct:getPartiesByIdTypeIdValueIdSubValue").routeId(ROUTE_ID_SUB).doTry()
+				.process(exchange -> {
+					requestCounterSub.inc(1); // increment Prometheus Counter metric
+					exchange.setProperty(TIMER_NAME_SUB, requestLatencySub.startTimer()); // initiate Prometheus Histogram metric
+				})
+				.to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
+						"'Request received, " + ROUTE_ID_SUB + "', null, null, null)") // default logging
+				/*
+				 * BEGIN processing
+				 */
+				.setBody(simple("{}"))
+				.removeHeaders("CamelHttp*")
+				.removeHeader(Exchange.HTTP_URI)
+				.setHeader("Content-Type", constant("application/json"))
+				.setHeader("Accept", constant("application/json"))
+				.setHeader(Exchange.HTTP_METHOD, constant("GET"))
+				.to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
+						"'Calling backend API, getParties', " +
+						"'Tracking the request', 'Track the response', " +
+						"'Request sent to, GET {{backend.endpoint}}/parties/${header.idType}/${header.idValue}/${header.idSubValue}')")
+				.toD("{{backend.endpoint}}/parties/${header.idType}/${header.idValue}/${header.idSubValue}?bridgeEndpoint=true&throwExceptionOnFailure=false")
+				.unmarshal().json(JsonLibrary.Gson)
+
+				// Add CORS headers
+				.process(corsFilter)
+
+				.to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
+						"'Response from backend API, getParties: ${body}', " +
+						"'Tracking the response', 'Verify the response', null)")
+				/*
+				 * END processing
+				 */
+				.to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
+						"'Send response, " + ROUTE_ID_SUB + "', null, null, 'Output Payload: ${body}')") // default logging
+				.doFinally().process(exchange -> {
+			((Histogram.Timer) exchange.getProperty(TIMER_NAME_SUB)).observeDuration(); // stop Prometheus Histogram metric
 		}).end()
 		;
 	}
