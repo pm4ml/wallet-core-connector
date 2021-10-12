f051b6d81817a30344f05ab3d8428256cab22b8c
Yordan Stoykov <yordan.stoykov@modusbox.com>
2021-10-12 15:50:13 +0300

    Added new error handling format and debug logging

--- core-connector/src/main/java/com/modusbox/client/processor/CustomErrorProcessor.java
+++ core-connector/src/main/java/com/modusbox/client/processor/CustomErrorProcessor.java
@@ -5,6 +5,7 @@ import com.modusbox.log4j2.message.CustomJsonMessageImpl;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.http.base.HttpOperationFailedException;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
@@ -37,21 +38,32 @@ public class CustomErrorProcessor implements Processor {
                     if (null != e.getResponseBody()) {
                         /* Below if block needs to be changed as per the error object structure specific to 
                             CBS back end API that is being integrated in Core Connector. */
+
+                        customJsonMessage.logJsonMessage("error", String.valueOf(exchange.getIn().getHeader("X-CorrelationId")),
+                                "Logging entire error object...", null, null,
+                                e.getResponseBody());
+
                         JSONObject respObject = new JSONObject(e.getResponseBody());
                         if (respObject.has("message")) {
 //                            statusCode = String.valueOf(respObject.getInt("returnCode"));
 //                            errorDescription = respObject.getString("returnStatus");
                             statusCode = String.valueOf(respObject.getInt("statusCode"));
-//                            statusCode = respObject.getString("statusCode");
-                            detailedDescription = respObject.getString("message");
-                            errorMessage = respObject.getJSONObject("transferState").getJSONObject("lastError").getJSONObject("mojaloopError").getJSONObject("errorInformation").getString("errorDescription");
+//                            statusCode = respObject.getString("statusCode");\
+                            // Replace 2 or more whitespace chars with just one
+                            detailedDescription = respObject.getString("message").replaceAll("\\s+", " ");
+                            try {
+                                errorMessage = respObject.getJSONObject("transferState").getJSONObject("lastError").getJSONObject("mojaloopError").getJSONObject("errorInformation").getString("errorDescription");
+                            } catch (JSONException ex) {
+//                                ex.printStackTrace();
+                                errorMessage = "Unknown - no mojaloopError message present";
+                            }
                         }
                     }
                 } finally {
                     reasonText = "{" +
                             "\"statusCode\": \"" + statusCode + "\"," +
-                            "\"message\": \"" + errorMessage + "\"" +
-//                            "\"detailedDescription\": \"" + detailedDescription +
+                            "\"message\": \"" + errorMessage + "\"," +
+                            "\"detailedDescription\": \"" + detailedDescription + "\"" +
                             "}";
                 }
             }
