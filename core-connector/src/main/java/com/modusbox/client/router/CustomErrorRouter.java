fd2daed7451248d2be1f83003dd53b971f637688
Juan Correa <gibaros@users.noreply.github.com>
2022-01-25 23:08:12 -0500

    mmd-1977: Add CheckMojaloopError

--- core-connector/src/main/java/com/modusbox/client/router/CustomErrorRouter.java
+++ core-connector/src/main/java/com/modusbox/client/router/CustomErrorRouter.java
@@ -1,15 +1,23 @@
 package com.modusbox.client.router;
 
 import com.modusbox.client.processor.CustomErrorProcessor;
+import com.modusbox.client.processor.CheckMojaloopError;
 import org.apache.camel.builder.RouteBuilder;
 
 public final class CustomErrorRouter extends RouteBuilder {
     private CustomErrorProcessor customErrorProcessor = new CustomErrorProcessor();
+    private CheckMojaloopError checkMojaloopError = new CheckMojaloopError();    
 
     public void configure() {
 
         from("direct:extractCustomErrors")
                 .process(customErrorProcessor)
+        ;
+        
+        
+        from("direct:catchMojaloopError")
+//                .process(exchange -> System.out.println())
+                .process(checkMojaloopError)
         ;
     }
 }
