51279ae27b5dfeba9ede8c7e487540c613f068c2
Juan Correa <gibaros@users.noreply.github.com>
2022-01-25 23:04:01 -0500

    mmd-1977: Update getTransfersByTransferId response

--- core-connector/src/main/java/com/modusbox/client/jaxrs/TransfersApiImpl.java
+++ core-connector/src/main/java/com/modusbox/client/jaxrs/TransfersApiImpl.java
@@ -2,7 +2,7 @@ package com.modusbox.client.jaxrs;
 
 import com.modusbox.client.api.TransfersApi;
 import com.modusbox.client.model.FulfilNotification;
-import com.modusbox.client.model.MojaloopTransferState;
+import com.modusbox.client.model.TransferStatusResponse;
 import com.modusbox.client.model.TransferRequestInbound;
 import com.modusbox.client.model.TransferResponseInbound;
 
@@ -12,7 +12,7 @@ import javax.validation.constraints.Pattern;
 public class TransfersApiImpl implements TransfersApi {
 
     @Override
-    public MojaloopTransferState getTransfersByTransferId(@Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$") String transferId) {
+    public TransferStatusResponse getTransfersByTransferId(@Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$") String transferId) {
         return null;
     }
 
